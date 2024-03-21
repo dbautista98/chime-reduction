66d41afd9ad81dc94ce2a6a83ef59f182a947f6b
Daniel <dbautista98@berkeley.edu>
2024-03-21 11:15:31 -0400

    added move_data.py

---
+++ move_data.py
@@ -0,0 +1,42 @@
+import glob
+import shutil
+import os
+from datetime import datetime
+
+def check_dir(filepath):
+    if os.path.exists(filepath):
+        return
+    else:
+        os.mkdir(filepath)
+        return
+
+
+def move_data():
+    all_dirs = glob.glob("*_*/")
+    data_dir = "/lustre/cv/projects/ESM/CHIME_data/"
+
+    try:
+        all_dirs.remove("__pycache__/")
+    except Exception:
+        pass
+
+    for date in all_dirs:
+        check_dir(f"{data_dir}/{date}")
+
+        contents = glob.glob(f"{date}/*")
+
+        for file in contents:
+            if os.path.exists(f"{data_dir}{file}"):
+                # print(f"FOUND: {data_dir}{file}")
+                os.remove(file)
+            else:
+                shutil.move(file,f"{data_dir}{file}")
+                # print(f"MISSING: {data_dir}{file}")
+                print(f"moved {file} to {data_dir}{file}")
+                # print()
+        
+        os.rmdir(date)
+
+if __name__ == "__main__":
+    move_data()
+    print(f"Job finished: {datetime.now().strftime('%Y-%m-%d at %H:%M:%S')}")
