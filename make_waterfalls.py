b093b259a68d646b0dfe06868a3c1fd8455b52cd
Daniel <dbautista98@berkeley.edu>
2025-02-20 09:53:40 -0500

    added option to switch between uncalibrated and calibrated plots

--- make_waterfalls.py
+++ make_waterfalls.py
@@ -30,8 +30,17 @@ def get_date(filepath):
     YYYY_DD = time_UTC.strftime("%Y_%j")
     return YYYY_DD
 
-def plot_waterfall(data_path, outdir="./"):
+def plot_waterfall(data_path, outdir="./", outtype="png", calibrated=False):
     CHIME_data, frequency, timestamps = calibration.load_CHIME_data(os.path.dirname(data_path))
+    if calibrated:
+        unit = "Jy"
+        CHIME_data = calibration.calibration(data_path)
+        vmin=1e4
+        vmax=7e5
+    else:
+        unit = "counts"
+        vmin=0
+        vmax=4e7
     start_time = timestamps[0]
     end_time = timestamps[-1]
 
@@ -44,15 +53,15 @@ def plot_waterfall(data_path, outdir="./"):
     (ax1, ax2), (ax3, ax4) = gs.subplots(sharex="col", sharey="row")
     ax1.set_title(f"GBO CHIME outrigger data\n{start_time.astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z')} to {end_time.astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z')}")
     ax1.semilogy(frequency, average_spectrum, color="black", linewidth=1)
-    ax1.set_ylabel("average power [counts]")
+    ax1.set_ylabel(f"average power [{unit}]")
     ax2.set_visible(not ax2)
-    ax3.imshow(CHIME_data, aspect="auto", extent=extent, vmin=0, vmax=4e7)
+    ax3.imshow(CHIME_data, aspect="auto", extent=extent, vmin=vmin, vmax=vmax)
     ax3.set_xlabel("Frequency [MHz]")
     ax3.set_ylabel("UTC time (mm-dd hh)")
     ax4.plot(time_series, timestamps, color="black", linewidth=1)
-    ax4.set_xlabel("integrated power\n[counts]")
-    plt.savefig(f"{outdir}/{start_time.strftime('%Y_%j')}_waterfall.png", bbox_inches="tight", transparent=False)
-    print(f"saved plot to: {outdir}/{start_time.strftime('%Y_%j')}_waterfall.png")
+    ax4.set_xlabel(f"integrated power\n[{unit}]")
+    plt.savefig(f"{outdir}/{start_time.strftime('%Y_%j')}_waterfall.{outtype}", bbox_inches="tight", transparent=False)
+    print(f"saved plot to: {outdir}/{start_time.strftime('%Y_%j')}_waterfall.{outtype}")
     plt.close()
 
 def move_files():
@@ -70,7 +79,7 @@ def move_files():
             print(f"moved {file} to {data_dir}{this_date}/{file}")
             shutil.move(file, data_dir + this_date + "/" + file)
 
-def make_waterfalls():
+def make_waterfalls(outtype='png', calibrated=False):
     dirs = glob.glob("*_*/")
     try:
         dirs.remove("__pycache__/")
@@ -78,14 +87,16 @@ def make_waterfalls():
         pass
 
     for date in dirs:
-        if os.path.exists(f"{date}/{date.replace('/', '')}_waterfall.png"):
+        if os.path.exists(f"{date}/{date.replace('/', '')}_waterfall.{outtype}"):
             pass
         else:
             contents = glob.glob(date + "/*npy")
             contents.sort()
-            plot_waterfall(date, outdir=f"{date}")
+            plot_waterfall(date, outdir=f"{date}", outtype=outtype, calibrated=calibrated)
 
 if __name__ == "__main__":
+    outtype = "png"
+    calibrated = False
     move_files()
-    make_waterfalls()
+    make_waterfalls(outtype=outtype, calibrated=calibrated)
     print(f"Job finished: {datetime.now().strftime('%Y-%m-%d at %H:%M:%S')}")
