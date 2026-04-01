c4f44469c80236ee36828cbc762977702e0b4678
Daniel Bautista <dbautista98@berkeley.edu>
2026-04-01 17:24:45 -0400

    rename directory

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='chime',
+    version='0.1.0',    
+    description='A python package to reduce CHIME data for ingestion into DRIFT]',
+    url='https://github.com/dbautista98/chime-reduction',
+    author='Daniel Bautista',
+    author_email='dbautist@nrao.edu',
+    packages=['chime'],
+    install_requires=[],
+
+)
